e3b23cb3e2752e9808aeb8cb68cbc2cb795d582e
lionelMaquet <lionel.maquet@gmail.com>
2019-01-21 22:13:14 +0100

    initial push

---
+++ menu.js
@@ -0,0 +1,25 @@
+$(".difficultyButton").on('click', function(e) {
+    
+    switch($(e.currentTarget).val()) {
+        case 'easy' : 
+            loadEasyVariables();
+            break;
+        case 'medium' :
+            loadNormalVariables();
+            break;
+        case 'hard' : 
+            loadHardVariables();
+            break; 
+    }
+})
+
+$(".soundButton").on('click', function(e) {
+    switch ($(e.currentTarget).val()) {
+        case 'soundOn' : 
+            soundOn = true;
+            break;
+        case 'soundOff' : 
+            soundOn = false;
+            break 
+    }
+})
